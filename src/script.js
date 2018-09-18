1cf08ac20716e404ae87b039d3c788817ec50cd0
banjac275 <banjac275@gmail.com>
2018-09-18 16:01:56 +0200

    added chart

--- src/script.js
+++ src/script.js
@@ -22,9 +22,9 @@ const recvData = serverList.map(el => {
 
 Promise.all(recvData)
 .then(data => {
-console.log(data)
-data.sort((a, b) => a.rank - b.rank)
-document.querySelector('.cs-table').innerHTML += `${data.map(el => 
+  console.log(data)
+  data.sort((a, b) => a.rank - b.rank)
+  document.querySelector('.cs-table').innerHTML += `${data.map(el => 
   `<tr>
     <td>${el.name}</td>
     <td>${el.ownerusername}</td>
@@ -37,6 +37,44 @@ document.querySelector('.cs-table').innerHTML += `${data.map(el =>
 })
 .catch(err => console.log(err))    
 
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }
+      ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 
 
 
